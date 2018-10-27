36c1d21637e41a7e436b9ec0900c1fd3848f3282
Artem Meleshko <>
2018-10-27 11:37:35 +0300

    added files

---
+++ CocoaTinyURL.h
@@ -0,0 +1,17 @@
+//
+//  CocoaTinyURL.h
+//  Everapp
+//
+//  Created by Artem on 10/27/18.
+//  Copyright © 2018 Everappz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CocoaTinyURL : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CocoaTinyURL.m
@@ -0,0 +1,13 @@
+//
+//  CocoaTinyURL.m
+//  Everapp
+//
+//  Created by Artem on 10/27/18.
+//  Copyright © 2018 Everappz. All rights reserved.
+//
+
+#import "CocoaTinyURL.h"
+
+@implementation CocoaTinyURL
+
+@end
